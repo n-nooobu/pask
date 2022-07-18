53fd2e4d868ed6563a1ba60b847ae954b3ddd810
n-nooobu <kamiya826@gmail.com>
2022-07-18 23:19:02 +0900

    Added: to be able to manage category.

--- pask/board.py
+++ pask/board.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import json
+from sre_constants import CATEGORY
 import string
 import random
 from datetime import datetime, date, timezone, timedelta
@@ -14,7 +15,7 @@ dash.register_page(__name__, path_template='/board/<board_name>')
 
 
 DATA_DIR = Path(f'{os.path.dirname(__file__)}/data')
-BOARD_DATA = None
+BOARD_DATA, CATEGORY_DATA, OPTIONS = None, None, None
 PREV_N1, PREV_N2, PREV_N_ADD_LIST, PREV_N_CARD_TO_LEFT, PREV_N_CARD_TO_RIGHT = 0, 0, 0, 0, 0
 PREV_N_ADD_CARD, PREV_N_ADD_ENTRY, PREV_N1_CARD, PREV_N2_CARD, PREV_N3_CARD = {}, {}, {}, {}, {}
 PREV_N_LIST_TO_LEFT, PREV_N_LIST_TO_RIGHT = None, None
@@ -22,6 +23,8 @@ PREV_N_LIST_TO_LEFT, PREV_N_LIST_TO_RIGHT = None, None
 
 def layout(board_name=None):
     global BOARD_DATA
+    global CATEGORY_DATA
+    global OPTIONS
     global PREV_N_LIST_TO_LEFT
     global PREV_N_LIST_TO_RIGHT
 
@@ -29,6 +32,9 @@ def layout(board_name=None):
         json_path = DATA_DIR/f'{board_name}.json'
         with open(json_path, 'r', encoding='utf-8') as f:
             BOARD_DATA = json.load(f)
+        with open(DATA_DIR/'category.json', 'r', encoding='utf-8') as f:
+            CATEGORY_DATA = json.load(f)
+        OPTIONS = [{'label': None, 'value': None}] + [{'label': f'{key}: {value["desc"]}', 'value': key} for key, value in CATEGORY_DATA.items()]
 
         lists = []
         all_modals = []
@@ -40,21 +46,35 @@ def layout(board_name=None):
             modals = []
             for card_id in card_ids:
                 card_data = BOARD_DATA['card_data'][card_id]
-                
-                now = datetime.now(timezone.utc).astimezone()
-                start = None
+
+                start_date, start_time, due_date, due_time = None, None, None, None
                 if card_data['start'] is not None:
                     start = datetime.fromisoformat(card_data['start']).astimezone()
-                due = None
+                    start_date, start_time = start.date(), start
+                    if not card_data['hasTime']:
+                        start_time = None
                 if card_data['due'] is not None:
                     due = datetime.fromisoformat(card_data['due']).astimezone()
+                    due_date, due_time = due.date(), due
+                    if not card_data['hasTime']:
+                        due_time = None
 
                 color = None
-                if due is not None:
-                    if now >= due:
-                        color = 'danger'
-                    elif timedelta(days=0) <= due - now <= timedelta(days=1):
-                        color = 'warning'
+                now = datetime.now(timezone.utc).astimezone()
+                if card_data['hasTime']:
+                    if due is not None:
+                        if now > due:
+                            color = 'danger'
+                        elif timedelta(days=0) <= due - now <= timedelta(days=1):
+                            color = 'warning'
+                else:
+                    now = now.date()
+                    if due_date is not None:
+                        if now > due_date:
+                            color = 'danger'
+                        elif timedelta(days=0) <= due_date - now <= timedelta(days=1):
+                            color = 'warning'
+
                 if card_data['check']:
                     color = 'success'
 
@@ -94,7 +114,7 @@ def layout(board_name=None):
                                         dcc.DatePickerRange(
                                             id={'type': 'entry-date-picker-range', 'index': entry_id}, className='date_picker_range',
                                             display_format='Y/M/D', month_format='Y/M',
-                                            initial_visible_month=now, start_date=start, end_date=due,
+                                            initial_visible_month=now, start_date=start, end_date=end,
                                         ),
                                     ]),
                                     dbc.Row([
@@ -102,7 +122,7 @@ def layout(board_name=None):
                                             dmc.TimeInput(id={'type': 'time-input-start-entry', 'index': entry_id}, value=start, class_name='time_input_in_card')
                                         ]),
                                         dbc.Col([
-                                            dmc.TimeInput(id={'type': 'time-input-end-entry', 'index': entry_id}, value=due, class_name='time_input_in_card')
+                                            dmc.TimeInput(id={'type': 'time-input-end-entry', 'index': entry_id}, value=end, class_name='time_input_in_card')
                                         ]),
                                     ]),
                                 ]),
@@ -120,6 +140,8 @@ def layout(board_name=None):
                             dbc.Input(id={'type': 'modal-card-name', 'index': card_id}, value=card_data['name'], className='card_board', type='text'),
                             html.H4('Description'),
                             dbc.Textarea(id={'type': 'modal-card-desc', 'index': card_id}, value=card_data['desc'], className='card_board'),
+                            html.H4('Category'),
+                            dbc.Select(id={'type': 'modal-card-category', 'index': card_id}, options=OPTIONS, value=card_data['category']),
                             html.H4('Due'),
                             dbc.Row([
                                 dbc.Col(width={'size': 'auto'}, children=[
@@ -130,15 +152,15 @@ def layout(board_name=None):
                                         dcc.DatePickerRange(
                                             id={'type': 'modal-card-date-picker-range', 'index': card_id}, className='date_picker_range',
                                             display_format='Y/M/D', month_format='Y/M',
-                                            initial_visible_month=now, start_date=start, end_date=due,
+                                            initial_visible_month=now, start_date=start_date, end_date=due_date,
                                         ),
                                     ]),
                                     dbc.Row([
                                         dbc.Col([
-                                            dmc.TimeInput(id={'type': 'time-input-start-modal-card', 'index': card_id}, value=start, class_name='time_input_in_card')
+                                            dmc.TimeInput(id={'type': 'time-input-start-modal-card', 'index': card_id}, value=start_time, class_name='time_input_in_card')
                                         ]),
                                         dbc.Col([
-                                            dmc.TimeInput(id={'type': 'time-input-due-modal-card', 'index': card_id}, value=due, class_name='time_input_in_card')
+                                            dmc.TimeInput(id={'type': 'time-input-due-modal-card', 'index': card_id}, value=due_time, class_name='time_input_in_card')
                                         ]),
                                     ]),
                                 ]),
@@ -231,6 +253,7 @@ def add_list_toggle_modal(n1, n2, is_open):
      State({'type': 'modal-card', 'index': MATCH}, 'id'),
      State({'type': 'card', 'index': MATCH}, 'children'),
      State({'type': 'card', 'index': MATCH}, 'color'),
+     State({'type': 'modal-card-category', 'index': MATCH}, 'value'),
      State({'type': 'modal-card-checkbox', 'index': MATCH}, 'value'),
      State({'type': 'modal-card-date-picker-range', 'index': MATCH}, 'start_date'),
      State({'type': 'modal-card-date-picker-range', 'index': MATCH}, 'end_date'),
@@ -238,12 +261,32 @@ def add_list_toggle_modal(n1, n2, is_open):
      State({'type': 'time-input-due-modal-card', 'index': MATCH}, 'value')],
     prevent_initial_call=True
 )
-def card_toggle_modal(n1, n2, n3, name, desc, is_open, card_id_dict, card_children, color, check, start_date, due_date, start_time, due_time):
+def card_toggle_modal(n1, n2, n3, name, desc, is_open, card_id_dict, card_children, color, category, check, start_date, due_date, start_time, due_time):
     global PREV_N1_CARD
     global PREV_N2_CARD
     global PREV_N3_CARD
 
     card_id = card_id_dict['index']
+
+    has_time = False
+    start, start_string = None, None
+    if start_date is not None:
+        start_date = datetime.fromisoformat(start_date).astimezone()
+        start = start_date
+        if start_time is not None:
+            has_time = True
+            start_time = datetime.fromisoformat(start_time).astimezone()
+            start = datetime(year=start_date.year, month=start_date.month, day=start_date.day, hour=start_time.hour, minute=start_time.minute, second=start_time.second, tzinfo=start_time.tzinfo)
+        start_string = start.isoformat()
+    due, due_string = None, None
+    if due_date is not None:
+        due_date = datetime.fromisoformat(due_date).astimezone()
+        due = due_date
+        if due_time is not None:
+            has_time = True
+            due_time = datetime.fromisoformat(due_time).astimezone()
+            due = datetime(year=due_date.year, month=due_date.month, day=due_date.day, hour=due_time.hour, minute=due_time.minute, second=due_time.second, tzinfo=due_time.tzinfo)
+        due_string = due.isoformat()
 
     if (n1 and card_id not in PREV_N1_CARD) or (card_id in PREV_N1_CARD and n1 > PREV_N1_CARD[card_id]):
         is_open = not is_open
@@ -251,38 +294,13 @@ def card_toggle_modal(n1, n2, n3, name, desc, is_open, card_id_dict, card_childr
     elif (n2 and card_id not in PREV_N2_CARD) or (card_id in PREV_N2_CARD and n2 > PREV_N2_CARD[card_id]):
         is_open = not is_open
 
-        now = datetime.now(timezone.utc).astimezone()
-        start, start_string = None, None
-        if start_date is not None:
-            start_date = datetime.fromisoformat(start_date).astimezone()
-            start = start_date
-            if start_time is not None:
-                start_time = datetime.fromisoformat(start_time).astimezone()
-                start = datetime(year=start_date.year, month=start_date.month, day=start_date.day, hour=start_time.hour, minute=start_time.minute, second=start_time.second, tzinfo=start_time.tzinfo)
-            start_string = start.isoformat()
-        due, due_string = None, None
-        if due_date is not None:
-            due_date = datetime.fromisoformat(due_date).astimezone()
-            due = due_date
-            if due_time is not None:
-                due_time = datetime.fromisoformat(due_time).astimezone()
-                due = datetime(year=due_date.year, month=due_date.month, day=due_date.day, hour=due_time.hour, minute=due_time.minute, second=due_time.second, tzinfo=due_time.tzinfo)
-            due_string = due.isoformat()
-
-        color = None
-        if due is not None:
-            if now >= due:
-                color = 'danger'
-            elif timedelta(days=0) <= due - now <= timedelta(days=1):
-                color = 'warning'
-        if check:
-            color = 'success'
-
         BOARD_DATA['card_data'][card_id]['name'] = name
         BOARD_DATA['card_data'][card_id]['desc'] = desc
+        BOARD_DATA['card_data'][card_id]['category'] = category
         BOARD_DATA['card_data'][card_id]['check'] = check
         BOARD_DATA['card_data'][card_id]['start'] = start_string
         BOARD_DATA['card_data'][card_id]['due'] = due_string
+        BOARD_DATA['card_data'][card_id]['hasTime'] = has_time
 
         card_children = [
             dbc.Row(justify='between', children=[
@@ -296,7 +314,24 @@ def card_toggle_modal(n1, n2, n3, name, desc, is_open, card_id_dict, card_childr
         ]
 
     elif (n3 and card_id not in PREV_N3_CARD) or (card_id in PREV_N3_CARD and n3 > PREV_N3_CARD[card_id]):
-        color, start_date, due_date, start_time, due_time = None, None, None, None, None
+        start_date, due_date, start_time, due_time, due = None, None, None, None, None
+        BOARD_DATA['card_data'][card_id]['start'] = None
+        BOARD_DATA['card_data'][card_id]['due'] = None
+        BOARD_DATA['card_data'][card_id]['hasTime'] = False
+    
+    color = None
+    if due is not None:
+        now = datetime.now(timezone.utc).astimezone()
+        if due.hour == 0 and due.minute == 0:
+            due = due.date()
+            now = now.date()
+        if now > due:
+            color = 'danger'
+        elif timedelta(days=0) <= due - now <= timedelta(days=1):
+            color = 'warning'
+
+    if check:
+        color = 'success'
 
     PREV_N1_CARD[card_id], PREV_N2_CARD[card_id], PREV_N3_CARD[card_id] = n1, n2, n3
 
@@ -457,6 +492,8 @@ def add_card(n_add_card, cards, list_id_dict, modals):
                     dbc.Input(id={'type': 'modal-card-name', 'index': new_card_id}, value='new card', className='card_board', type='text'),
                     html.H4('Description'),
                     dbc.Textarea(id={'type': 'modal-card-desc', 'index': new_card_id}, value='', className='card_board'),
+                    html.H4('Category'),
+                    dbc.Select(id={'type': 'modal-card-category', 'index': new_card_id}, options=OPTIONS),
                     html.H4('Due'),
                     dbc.Row([
                         dbc.Col(width={'size': 'auto'}, children=[
@@ -495,9 +532,11 @@ def add_card(n_add_card, cards, list_id_dict, modals):
         BOARD_DATA['card_data'][new_card_id] = {
             'name': 'new card',
             'desc': '',
+            'category': '',
             'check': False,
             'start': None,
             'due': None,
+            'hasTime': False,
         }
         BOARD_DATA['card'][list_id].append(new_card_id)
         BOARD_DATA['entry'][new_card_id] = []
